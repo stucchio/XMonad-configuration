d51f2fd0e6ff5d247fec7f2fcf04fe94b3519073
Chris Stucchio <stucchio@gmail.com>
2010-09-14 07:44:59 -0400

    Added support for Totem movie player to work in full screen.

--- xmonad.hs
+++ xmonad.hs
@@ -11,11 +11,14 @@ import XMonad.Prompt.Shell
 -- Layouts
 import XMonad.Layout.Roledex
 import XMonad.Layout.Accordion
-import XMonad.Hooks.ManageHelpers (isFullscreen,doFullFloat)
+import XMonad.Hooks.ManageHelpers
 import XMonad.Config.Desktop (desktopLayoutModifiers)
 
 import qualified XMonad.StackSet as W
+
 import qualified Data.Map        as M
+import Monad
+import Data.Monoid (All (All))
 
 myModMask = mod4Mask
 noKeyModifier = 0
@@ -104,10 +107,41 @@ myManageHook = composeAll
 
 newManageHook = myManageHook <+> manageHook defaultConfig
 
+-- Helper functions to fullscreen the window
+fullFloat, tileWin :: Window -> X ()
+fullFloat w = windows $ W.float w r
+    where r = W.RationalRect 0 0 1 1
+tileWin w = windows $ W.sink w
+
+totemEventHook :: Event -> X All
+totemEventHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
+  state <- getAtom "_NET_WM_STATE"
+  fullsc <- getAtom "_NET_WM_STATE_FULLSCREEN"
+  isFull <- runQuery isFullscreen win
+  -- Constants for the _NET_WM_STATE protocol
+  let remove = 0
+      add = 1
+      toggle = 2
+      -- The ATOM property type for changeProperty
+      ptype = 4
+      action = head dat
+  when (typ == state && (fromIntegral fullsc) `elem` tail dat) $ do
+    when (action == add || (action == toggle && not isFull)) $ do
+         io $ changeProperty32 dpy win state ptype propModeReplace [fromIntegral fullsc]
+         fullFloat win
+    when (head dat == remove || (action == toggle && isFull)) $ do
+         io $ changeProperty32 dpy win state ptype propModeReplace []
+         tileWin win
+  -- It shouldn't be necessary for xmonad to do anything more with this event
+  return $ All False
+
+evHook _ = return $ All True
+
 main = xmonad $ gnomeConfig { terminal     = "gnome-terminal"
                             , modMask      = myModMask
                             , keys         = myKeys
                             , layoutHook   = ( desktopLayoutModifiers myLayout )
                             , manageHook = newManageHook
                             , startupHook = setWMName "LG3D"
+                            , handleEventHook = totemEventHook
                           }
